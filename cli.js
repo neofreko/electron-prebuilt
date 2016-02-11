3530b93c9eb117d3e63d63d997c5cada97f42ac6
Akhmad Fathonih <akhmadf@gmail.com>
2016-02-11 09:57:18 +0900

    Update cli.js
    debug

--- cli.js
+++ cli.js
@@ -8,3 +8,7 @@ var child = proc.spawn('xvfb-run', [].concat(electron, process.argv.slice(2)), {
 child.on('close', function (code) {
   process.exit(code)
 })
+
+child.on('error', (err) => {
+  console.log('Failed to start child process.', ['xvfb-run'].concat(electron, process.argv.slice(2)));
+});
