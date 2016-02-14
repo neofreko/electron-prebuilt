88b4ac9061765b6e55eedcd14c26b75888dd3219
Akhmad Fathonih <akhmadf@gmail.com>
2016-02-14 16:38:49 +0900

    hardcode LD_LIBRARY_PATH

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 var electron = require('./')
 
 var proc = require('child_process')
-
+process.env.LD_LIBRARY_PATH = '/app/.apt/usr/lib/x86_64-linux-gnu:/app/.apt/usr/lib/i386-linux-gnu:/app/.apt/usr/lib'
 var child = proc.spawn('xvfb-run', [].concat(electron, process.argv.slice(2)), {stdio: 'inherit'})
 child.on('close', function (code) {
   process.exit(code)
